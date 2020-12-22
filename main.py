c27cc4cbf6a38d131bbef5d740e9fd2f6f3cdd00
matthewqian1 <matthew.qian2@gmail.com>
2020-12-22 18:38:19 +1100

    final

--- main.py
+++ main.py
@@ -17,7 +17,6 @@ pygame.display.set_icon(icon)
 # background
 background = pygame.image.load('snow.png')
 
-#hello
 # background music
 mixer.music.load('414673__lena-orsa__a-christmas-tale.mp3')
 mixer.music.play(-1)
@@ -37,6 +36,7 @@ class Player(object):
         self.x_change = x_change
 
 
+
 class Enemy(object):
     def __init__(self, x, y, x_change, y_change):
         self.x = x
@@ -45,22 +45,21 @@ class Enemy(object):
         self.y_change = y_change
 
 
+# enemy
+enemyImg = pygame.image.load('grinch.png')
+num_enemies = 12
+enemies = []
+for i in range(num_enemies):
+    new_enemy = Enemy(random.randint(0, 735), 80, 1, 50)
+    enemies.append(new_enemy)
+
+
 class Boss(object):
     def __init__(self, x, y, hp, x_change):
         self.x = x
         self.y = y
         self.hp = hp
         self.x_change = x_change
-
-
-# enemy
-enemyImg = pygame.image.load('grinch.png')
-num_enemies = 12
-enemies = []
-for i in range(num_enemies):
-    # enemyImg.append(pygame.image.load('grinch.png'))
-    new_enemy = Enemy(random.randint(0, 735), 80, 1, 50)
-    enemies.append(new_enemy)
 
 
 class Bullet(object):
@@ -86,7 +85,6 @@ class Rock(object):
 
 
 santaImg = pygame.image.load('santaIcon.png')
-
 
 
 class Santa(object):
@@ -117,6 +115,9 @@ def game_over_test():
     over_text = over_font.render('GAME OVER', True, (255, 255, 255))
     screen.blit(over_text, (200, 250))
 
+def game_won():
+    won_text = over_font.render('YOU WIN', True, (255, 255, 0))
+    screen.blit(won_text, (200, 250))
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -124,9 +125,9 @@ def player(x, y):
 
 def show_boss(x, y, hp):
     bossImg = pygame.image.load('grinchboss.png')
-    if 80 >= hp > 60:
+    if 64 >= hp > 32:
         bossImg = pygame.image.load('grinch3.png')
-    elif hp <= 60:
+    elif hp <= 32:
         bossImg = pygame.image.load('grinchlast.png')
     screen.blit(bossImg, (x, y))
     pygame.draw.rect(screen, (255, 0, 0), (x + 20, y - 5, 50, 10))
@@ -151,7 +152,7 @@ def fire_bullet(x, y):
 
 def isCollision(object1_x, object1_y, object2_x, object2_y, hitbox):
     dist = math.sqrt(math.pow(object1_x - object2_x, 2) + math.pow(object1_y - object2_y, 2))
-    if object1_x < object2_x:
+    if object1_x > object2_x:
         if dist < hitbox:
             return True
         else:
@@ -175,7 +176,6 @@ numRocks = math.floor(width / 100)
 santa = Santa(0, 530, 5)
 take_dmg = mixer.Sound('dmg.wav')
 game_over = False
-wait_page = True
 waitImg = pygame.image.load('wait_page.png')
 # game loop
 running = True
@@ -187,9 +187,9 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                player1.x_change = -2
+                player1.x_change = -2.5
             if event.key == pygame.K_RIGHT:
-                player1.x_change = 2
+                player1.x_change = 2.5
             if event.key == pygame.K_SPACE:
                 if bullet.state == 'ready':
                     bullet_sound = mixer.Sound('427595__michorvath__20-gauge-shotgun-gunshot.wav')
@@ -208,110 +208,124 @@ while running:
         player1.x = 736
 
     # Level 1
-    if boss.hp > 80:
-        for i in range(num_enemies):
-            # game over
-            if enemies[i].y > 600:
-                santa.hp -= 1
+    if game_over == False:
+        if boss.hp > 64:
+            for i in range(num_enemies):
+                # game over
+                if enemies[i].y > 600:
+                    santa.hp -= 1
 
-            enemies[i].x += enemies[i].x_change
-            if enemies[i].x <= 0:
-                enemies[i].x_change = 1
-                enemies[i].y += enemies[i].y_change
-            elif enemies[i].x >= 736:
-                enemies[i].x_change = -1
-                enemies[i].y += enemies[i].y_change
+                enemies[i].x += enemies[i].x_change
+                if enemies[i].x <= 0:
+                    enemies[i].x_change = 1
+                    enemies[i].y += enemies[i].y_change
+                elif enemies[i].x >= 736:
+                    enemies[i].x_change = -1
+                    enemies[i].y += enemies[i].y_change
 
-            # collision
-            collision = isCollision(enemies[i].x, enemies[i].y, bullet.x, bullet.y, 25)
-            if collision:
-                explosion_sound = mixer.Sound('344276__nsstudios__laser3.wav')
-                explosion_sound.play()
-                bullet.y = 480
-                bullet.state = 'ready'
-                boss.hp -= 10
-                enemies[i].x = random.randint(0, 735)
-                enemies[i].y = 80
-            enemy(enemies[i].x, enemies[i].y)
+                # collision
+                collision = isCollision(bullet.x, bullet.y, enemies[i].x, enemies[i].y, 20)
+                if collision:
+                    explosion_sound = mixer.Sound('344276__nsstudios__laser3.wav')
+                    explosion_sound.play()
+                    bullet.y = 480
+                    bullet.state = 'ready'
+                    boss.hp -= 3
+                    enemies[i].x = random.randint(0, 735)
+                    enemies[i].y = -100
+                    enemies[i].x_change = 0
+                enemy(enemies[i].x, enemies[i].y)
 
-    # level 2
-    elif boss.hp > 60:
-        if not level_up:
-            laugh_sound = mixer.Sound('laugh.wav')
-            laugh_sound.play()
-            boss.x_change = 2
-            level_up = True
+        # level 2
+        elif boss.hp > 32:
+            if not level_up:
+                laugh_sound = mixer.Sound('laugh.wav')
+                laugh_sound.play()
+                boss.x_change = 2
+                level_up = True
 
-        if boss.x <= 0:
-            boss.x_change = 1.5
-        elif boss.x >= 736:
-            boss.x_change = -1.5
+            if boss.x <= 0:
+                boss.x_change = 1.5
+            elif boss.x >= 736:
+                boss.x_change = -1.5
 
-        rotate = random.randint(1, 2)
-        if boss.x % 100 == 0 and rotate == 2:
-            boss.x_change *= -1
-            missileImg.append(pygame.image.load('giftbox.png'))
-            missileX.append(boss.x)
-            missileY.append(20)
-            missileY_change.append(2.5)
-        for i in range(len(missileImg)):
-            missileY[i] += missileY_change[i]
-            missile(missileX[i], missileY[i], i)
-            collect = isCollision(player1.x, player1.y, missileX[i], missileY[i], 25)
-            if collect:
-                success = mixer.Sound('collect.wav')
-                success.play()
-                missileY[i] = 2000
-                boss.hp -= 10
-        boss.x += boss.x_change
-        if boss.hp == 60:
-            level_up = False
+            rotate = random.randint(1, 2)
+            if boss.x % 100 == 0 and rotate == 2:
+                boss.x_change *= -1
+                missileImg.append(pygame.image.load('giftbox.png'))
+                missileX.append(boss.x)
+                missileY.append(20)
+                missileY_change.append(2.5)
+            for i in range(len(missileImg)):
+                missileY[i] += missileY_change[i]
+                missile(missileX[i], missileY[i], i)
+                collect = isCollision(player1.x, player1.y, missileX[i], missileY[i], 25)
+                if collect:
+                    success = mixer.Sound('collect.wav')
+                    success.play()
+                    missileX[i] = 2000
+                    boss.hp -= 4
+                if 735 >= missileX[i] >= 0 and missileY[i] == 600:
+                    santa.hp -= 1
+            boss.x += boss.x_change
+            if boss.hp == 32:
+                level_up = False
 
-    # lv3
-    else:
-        boss.x_change = 0
-        boss.x = 690
-        boss.y = 10
-        if not level_up:
-            laugh_sound = mixer.Sound('donotmess.mp3')
-            laugh_sound.play()
-            boss.x_change = 2
-            level_up = True
-        if not rock_launch:
-            opening = random.randint(1, 7)
-            for i in range(1, numRocks):
-                if i != opening:
-                    rock = Rock(i * 100 - 50, boss.y + 50, 1.5)
-                    rocks.append(rock)
-            rock_launch = True
-        for j in range(len(rocks)):
-            rocks[j].y += rocks[j].y_change
-            Rock.draw(rocks[j], screen)
-            hit = isCollision(player1.x, player1.y, rocks[j].x, rocks[j].y, 50)
-            if hit:
-                santa.hp -= 1
-                take_dmg.play()
-                rocks[0].y = 600
+        # lv3
+        else:
+            boss.x_change = 0
+            boss.x = 690
+            boss.y = 10
+            if not level_up:
+                laugh_sound = mixer.Sound('donotmess.mp3')
+                laugh_sound.play()
+                boss.x_change = 2
+                level_up = True
+            if not rock_launch:
+                opening = random.randint(1, 7)
+                for i in range(1, numRocks):
+                    if i != opening:
+                        rock = Rock(i * 100 - 50, boss.y + 50, 1.5)
+                        rocks.append(rock)
+                rock_launch = True
+            for j in range(len(rocks)):
+                rocks[j].y += rocks[j].y_change
+                Rock.draw(rocks[j], screen)
+                hit = isCollision(rocks[j].x, rocks[j].y, player1.x, player1.y, 50)
+                if hit:
+                    santa.hp -= 1
+                    take_dmg.play()
+                    rocks[0].y = 600
 
-        if rock_launch is True and rocks[0].y == 600:
-            rocks.clear()
-            rock_launch = False
+            if rock_launch is True and rocks[0].y == 600:
+                boss.hp -= 4
+                rocks.clear()
+                rock_launch = False
 
-    if bullet.y <= 0:
-        bullet.y = 480
-        bullet.state = 'ready'
+        if bullet.y <= 0:
+            bullet.y = 480
+            bullet.state = 'ready'
 
-    if bullet.state is 'fire':
-        fire_bullet(bullet.x, bullet.y)
-        bullet.y -= bullet.y_change
+        if bullet.state == 'fire':
+            fire_bullet(bullet.x, bullet.y)
+            bullet.y -= bullet.y_change
 
-    player(player1.x, player1.y)
-    show_boss(boss.x, boss.y, boss.hp)
-    Santa.draw(santa, screen)
+    if game_over == False:
+        player(player1.x, player1.y)
+        show_boss(boss.x, boss.y, boss.hp)
+        Santa.draw(santa, screen)
+    if boss.hp <= 0:
+        game_over = True
+        game_won()
+        if boss.hp == 0:
+            win_sound = mixer.Sound('wingame.mp3')
+            win_sound.play()
+            boss.hp -= 1
     if santa.hp <= 0:
         game_over = True
-        player1.x = -1000
-        screen.fill((0, 0, 0))
-        screen.blit(background, (0, 0))
         game_over_test()
+        if santa.hp == 0:
+            lose_sound = mixer.Sound('lose.wav')
+            lose_sound.play()
+            santa.hp -= 1
     pygame.display.update()
